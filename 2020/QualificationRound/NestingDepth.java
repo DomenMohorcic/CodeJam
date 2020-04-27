e392402e55711f16484eb34905dce6739d939f54
Domen Mohorcic <d.mohorcic@gmail.com>
2020-04-27 21:33:45 +0200

    Vesigium and NestingDepth problems

---
+++ 2020/QualificationRound/NestingDepth.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class NestingDepth {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		String s, sol = "";
+		for(int a = 1; a <= t; a++) {
+			s = sc.next();
+			sol = "";
+			int prev_num = 0, brackets = 0;
+			for(int i = 0; i < s.length(); i++) {
+				int c = Character.getNumericValue(s.charAt(i));
+				if(prev_num > c) { //)
+					for(int j = 0; j < prev_num-c; j++) {
+						sol += ')';
+						brackets--;
+					}
+				} else if(prev_num < c) { //(
+					for(int j = 0; j < c-prev_num; j++) {
+						sol += '(';
+						brackets++;
+					}
+				}
+				sol += s.charAt(i);
+				prev_num = c;
+			}
+			for(int j = 0; j < brackets; j++) {
+				sol += ')';
+			}
+			System.out.printf("Case #%d: %s\n", a, sol);
+		}
+	}
+}
---
+++ 2020/QualificationRound/Vestigium.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class Vestigium {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tests = sc.nextInt();
+		for(int a = 1; a <= tests; a++) {
+			int size = sc.nextInt();
+			int[][] array = new int[size][size];
+			int[][][] duplicates = new int[2][size][size]; //0..rows, 1...cols
+			int trace = 0, dup_cols = 0, dup_rows = 0;
+			for(int i = 0; i < size; i++) {
+				for(int j = 0; j < size; j++) {
+					array[i][j] = sc.nextInt();
+					if(i == j) trace += array[i][j];
+					duplicates[0][i][array[i][j]-1]++;
+					duplicates[1][j][array[i][j]-1]++;
+				}
+			}
+			for(int i = 0; i < size; i++) {
+				for(int j = 0; j < size; j++) {
+					if(duplicates[0][i][j] > 1) {
+						dup_rows++;
+						break;
+					}
+				}
+				for(int j = 0; j < size; j++) {
+					if(duplicates[1][i][j] > 1) {
+						dup_cols++;
+						break;
+					}
+				}
+			}
+			System.out.printf("Case #%d: %d %d %d\n", a, trace, dup_rows, dup_cols);
+		}
+	}
+}
