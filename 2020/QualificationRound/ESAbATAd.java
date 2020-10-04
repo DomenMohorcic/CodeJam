08ee550fd6f7e85aee85f6d3bd5bff52a1737164
Domen Mohorcic <d.mohorcic@gmail.com>
2020-10-04 23:30:43 +0200

    Wrote judge for ESAbATAd problem

--- 2020/QualificationRound/ESAbATAd.java
+++ 2020/QualificationRound/ESAbATAd.java
@@ -7,65 +7,66 @@ public class ESAbATAd {
 		int tests = sc.nextInt();
 		int b = sc.nextInt();
 		for(int a = 1; a <= tests; a++) {
-			int[] bits = new int[b];
+			boolean[] bits = new boolean[b];
 			int idx_left = 0, idx_right = b-1;
 			boolean first_round = true;
 			while(true) {
-				if(idx_left > idx_right) { //check every 10 questions if we found all
+				if(idx_left > idx_right) { // check every 10 questions if we found all
 					if(guess(bits, sc)) break;
 					else System.exit(0);
 				}
-				if(first_round) { //ask about 10 positions
+				if(first_round) { // ask about 10 positions
 					for(int i = 0; i < 5; i++) {
 						System.out.println(idx_left+1);
-						bits[idx_left] = sc.nextInt();
+						bits[idx_left] = sc.nextInt() == 1 ? true : false;
 						idx_left++;
 					}
 					for(int i = 0; i < 5; i++) {
 						System.out.println(idx_right+1);
-						bits[idx_right] = sc.nextInt();
+						bits[idx_right] = sc.nextInt() == 1 ? true : false;
 						idx_right--;
 					}
 					first_round = false;
 				} else {
-					//figure out what happened
+					// figure out what happened
 					boolean invert = false, swap = false;
-					int tmp, asked = 0;
-					int[] pos = findSuitable(bits, idx_left);
-					if(pos[0] != -1) {
+					boolean tmp;
+					int asked = 0;
+					int[] pos = findSuitableIdx(bits, idx_left);
+					if(pos[0] != -1) { // same bits
 						System.out.println(pos[0]+1);
-						tmp = sc.nextInt();
+						tmp = sc.nextInt() == 1 ? true : false;
 						asked++;
 						if(tmp != bits[pos[0]]) invert = true;
 					}
-					if(pos[1] != -1) {
+					if(pos[1] != -1) { // different bits
 						System.out.println(pos[1]+1);
-						tmp = sc.nextInt();
+						tmp = sc.nextInt() == 1 ? true : false;
 						asked++;
 						if(invert && tmp == bits[pos[1]] || !invert && tmp != bits[pos[1]]) swap = true;
 					}
 					bits = modify(bits, invert, swap);
-					//ask remaining questions
-					if(asked%2 == 0) {
-						for(int i = 0; i < 5-asked/2; i++) {
-							System.out.println(idx_left+1);
-							bits[idx_left] = sc.nextInt();
-							idx_left++;
-						}
-						for(int i = 0; i < 5-asked/2; i++) {
-							System.out.println(idx_right+1);
-							bits[idx_right] = sc.nextInt();
-							idx_right--;
-						}
-					} else {
+					// ask remaining questions
+					if(asked == 2) {
 						for(int i = 0; i < 4; i++) {
 							System.out.println(idx_left+1);
-							bits[idx_left] = sc.nextInt();
+							bits[idx_left] = sc.nextInt() == 1 ? true : false;
+							idx_left++;
+						}
+						for(int i = 0; i < 4; i++) {
+							System.out.println(idx_right+1);
+							bits[idx_right] = sc.nextInt() == 1 ? true : false;
+							idx_right--;
+						}
+					} else { // ko vprasa 1 vprasanje gre 5 bit narobe
+						for(int i = 0; i < 4; i++) {
+							System.out.println(idx_left+1);
+							bits[idx_left] = sc.nextInt() == 1 ? true : false;
 							idx_left++;
 						}
 						for(int i = 0; i < 5; i++) {
 							System.out.println(idx_right+1);
-							bits[idx_right] = sc.nextInt();
+							bits[idx_right] = sc.nextInt() == 1 ? true : false;
 							idx_right--;
 						}
 					}
@@ -74,27 +75,27 @@ public class ESAbATAd {
 		}
 	}
 
-	private static boolean guess(int[] b, Scanner sc) {
-		for(int i = 0; i < b.length; i++) System.out.print(b[i]);
+	private static boolean guess(boolean[] b, Scanner sc) {
+		for(int i = 0; i < b.length; i++) System.out.print(b[i] ? 1 : 0);
 		System.out.println();
 		String result = sc.next();
 		if(result.equals("N")) return false;
 		return true;
 	}
 
-	private static int[] modify(int[] b, boolean invert, boolean swap) {
-		int[] a = new int[b.length];
+	private static boolean[] modify(boolean[] b, boolean invert, boolean swap) {
+		boolean[] a = new boolean[b.length];
 		if(swap) {
-			if(invert) for(int i = 0; i < b.length; i++) a[i] = b[b.length-1-i] == 1 ? 0 : 1;
+			if(invert) for(int i = 0; i < b.length; i++) a[i] = !b[b.length-1-i];
 			else for(int i = 0; i < b.length; i++) a[i] = b[b.length-1-i];
 		} else {
-			if(invert) for(int i = 0; i < b.length; i++) a[i] = b[i] == 1 ? 0 : 1;
+			if(invert) for(int i = 0; i < b.length; i++) a[i] = !b[i];
 			else return b;
 		}
 		return a;
 	}
 
-	private static int[] findSuitable(int[] b, int idx_left) {
+	private static int[] findSuitableIdx(boolean[] b, int idx_left) {
 		int same = -1, diff = -1;
 		for(int i = 0; i < idx_left; i++) {
 			if(same == -1 && b[i] == b[b.length-1-i]) same = i;
---
+++ 2020/QualificationRound/ESAbATAd_judge.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class ESAbATAd_judge {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = (int) Math.round(Math.random()*99+1);
+		int b = 30;/* (int) Math.round(Math.random()*9+1)*10; */
+		System.out.println(t);
+		System.out.println(b);
+		for(int a = 1; a <= t; a++) {
+			boolean[] bits = newRandomBits(b);
+			int asked_questions = 0;
+			while(asked_questions < 150) {
+				String s = sc.next();
+				if(s.length() == b) {
+					for(int i = 0; i < b; i++) {
+						if(bits[i] && s.charAt(i) == '0' || !bits[i] && s.charAt(i) == '1') {
+							System.out.println('N');
+							System.err.println("["+a+"/"+t+"]Submited");
+							System.err.println(s);
+							System.err.println("["+a+"/"+t+"]Real");
+							System.err.println(toString(bits));
+							System.exit(1);
+						}
+					}
+					System.out.println('Y');
+					break;
+				} else {
+					asked_questions++;
+					if(asked_questions%10 == 1) {
+						bits = randomPermutation(bits);
+					}
+					int idx = Integer.parseInt(s);
+					System.out.println(bits[idx-1] ? 1 : 0);
+				}
+			}
+		}
+	}
+
+	private static String toString(boolean[] bits) {
+		String s = "";
+		for(boolean b : bits) {
+			s += b ? '1' : '0';
+		}
+		return s;
+	}
+
+	private static boolean[] randomPermutation(boolean[] b) {
+		boolean[] a = new boolean[b.length];
+		boolean swap = Math.random() > 0.5;
+		boolean invert = Math.random() > 0.5;
+		if(swap) {
+			if(invert) for(int i = 0; i < b.length; i++) a[i] = !b[b.length-1-i];
+			else for(int i = 0; i < b.length; i++) a[i] = b[b.length-1-i];
+		} else {
+			if(invert) for(int i = 0; i < b.length; i++) a[i] = !b[i];
+			else return b;
+		}
+		return a;
+	}
+
+	private static boolean[] newRandomBits(int b) {
+		boolean[] a = new boolean[b];
+		for(int i = 0; i < b; i++) a[i] = Math.random() > 0.5;
+		return a;
+	}
+}
