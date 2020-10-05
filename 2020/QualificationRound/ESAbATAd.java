294b612456546506b26175ad4bc36234a24191f1
Domen Mohorcic <d.mohorcic@gmail.com>
2020-10-05 21:38:41 +0200

    Working ESAb_ATAd

--- 2020/QualificationRound/ESAbATAd.java
+++ 2020/QualificationRound/ESAbATAd.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class ESAbATAd {
 
+	static int index = 0;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int tests = sc.nextInt();
@@ -10,6 +12,7 @@ public class ESAbATAd {
 			boolean[] bits = new boolean[b];
 			int idx_left = 0, idx_right = b-1;
 			boolean first_round = true;
+			index = 0;
 			while(true) {
 				if(idx_left > idx_right) { // check every 10 questions if we found all
 					if(guess(bits, sc)) break;
@@ -18,12 +21,12 @@ public class ESAbATAd {
 				if(first_round) { // ask about 10 positions
 					for(int i = 0; i < 5; i++) {
 						System.out.println(idx_left+1);
-						bits[idx_left] = sc.nextInt() == 1 ? true : false;
+						bits[idx_left] = sc.nextInt() == 1;
 						idx_left++;
 					}
 					for(int i = 0; i < 5; i++) {
 						System.out.println(idx_right+1);
-						bits[idx_right] = sc.nextInt() == 1 ? true : false;
+						bits[idx_right] = sc.nextInt() == 1;
 						idx_right--;
 					}
 					first_round = false;
@@ -35,40 +38,31 @@ public class ESAbATAd {
 					int[] pos = findSuitableIdx(bits, idx_left);
 					if(pos[0] != -1) { // same bits
 						System.out.println(pos[0]+1);
-						tmp = sc.nextInt() == 1 ? true : false;
+						tmp = sc.nextInt() == 1;
 						asked++;
 						if(tmp != bits[pos[0]]) invert = true;
 					}
 					if(pos[1] != -1) { // different bits
 						System.out.println(pos[1]+1);
-						tmp = sc.nextInt() == 1 ? true : false;
+						tmp = sc.nextInt() == 1;
 						asked++;
-						if(invert && tmp == bits[pos[1]] || !invert && tmp != bits[pos[1]]) swap = true;
+						if((invert && tmp == bits[pos[1]]) || (!invert && tmp != bits[pos[1]])) swap = true;
+					}
+					if(asked == 1) { // blank reading
+						System.out.println(1);
+						int u = sc.nextInt();
 					}
 					bits = modify(bits, invert, swap);
 					// ask remaining questions
-					if(asked == 2) {
-						for(int i = 0; i < 4; i++) {
-							System.out.println(idx_left+1);
-							bits[idx_left] = sc.nextInt() == 1 ? true : false;
-							idx_left++;
-						}
-						for(int i = 0; i < 4; i++) {
-							System.out.println(idx_right+1);
-							bits[idx_right] = sc.nextInt() == 1 ? true : false;
-							idx_right--;
-						}
-					} else { // ko vprasa 1 vprasanje gre 5 bit narobe
-						for(int i = 0; i < 4; i++) {
-							System.out.println(idx_left+1);
-							bits[idx_left] = sc.nextInt() == 1 ? true : false;
-							idx_left++;
-						}
-						for(int i = 0; i < 5; i++) {
-							System.out.println(idx_right+1);
-							bits[idx_right] = sc.nextInt() == 1 ? true : false;
-							idx_right--;
-						}
+					for(int i = 0; i < 4; i++) {
+						System.out.println(idx_left+1);
+						bits[idx_left] = sc.nextInt() == 1;
+						idx_left++;
+					}
+					for(int i = 0; i < 4; i++) {
+						System.out.println(idx_right+1);
+						bits[idx_right] = sc.nextInt() == 1;
+						idx_right--;
 					}
 				}
 			}
@@ -85,6 +79,7 @@ public class ESAbATAd {
 
 	private static boolean[] modify(boolean[] b, boolean invert, boolean swap) {
 		boolean[] a = new boolean[b.length];
+		//System.err.println("["+(index++)+"] Swap: "+swap+", invert: "+invert);
 		if(swap) {
 			if(invert) for(int i = 0; i < b.length; i++) a[i] = !b[b.length-1-i];
 			else for(int i = 0; i < b.length; i++) a[i] = b[b.length-1-i];
--- 2020/QualificationRound/ESAbATAd_judge.java
+++ 2020/QualificationRound/ESAbATAd_judge.java
@@ -1,24 +1,27 @@
 import java.util.Scanner;
 
 public class ESAbATAd_judge {
+
+	static int index = -1;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int t = (int) Math.round(Math.random()*99+1);
-		int b = 30;/* (int) Math.round(Math.random()*9+1)*10; */
+		int b = (int) Math.round(Math.random()*9+1)*10;
 		System.out.println(t);
 		System.out.println(b);
 		for(int a = 1; a <= t; a++) {
 			boolean[] bits = newRandomBits(b);
 			int asked_questions = 0;
+			index = -1;
 			while(asked_questions < 150) {
 				String s = sc.next();
 				if(s.length() == b) {
 					for(int i = 0; i < b; i++) {
 						if(bits[i] && s.charAt(i) == '0' || !bits[i] && s.charAt(i) == '1') {
 							System.out.println('N');
-							System.err.println("["+a+"/"+t+"]Submited");
+							System.err.println("["+a+"/"+t+"] Submited");
 							System.err.println(s);
-							System.err.println("["+a+"/"+t+"]Real");
+							System.err.println("["+a+"/"+t+"] Real");
 							System.err.println(toString(bits));
 							System.exit(1);
 						}
@@ -49,6 +52,7 @@ public class ESAbATAd_judge {
 		boolean[] a = new boolean[b.length];
 		boolean swap = Math.random() > 0.5;
 		boolean invert = Math.random() > 0.5;
+		//System.err.println("["+(index++)+"] Swap: "+swap+", invert: "+invert);
 		if(swap) {
 			if(invert) for(int i = 0; i < b.length; i++) a[i] = !b[b.length-1-i];
 			else for(int i = 0; i < b.length; i++) a[i] = b[b.length-1-i];
